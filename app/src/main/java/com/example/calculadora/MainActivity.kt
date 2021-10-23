58540cdd320b7fd0f1ada751b57f05a18b4ff473
Johann Benjamin Duran Ticona <johannduranticona12@gmail.com>
2021-10-22 23:27:03 -0300

    Adição da Lógica

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,22 +2,51 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import android.widget.RadioButton
-import android.widget.TextView
+import com.example.calculadora.databinding.ActivityMainBinding
+
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.Soma.setOnClickListener{calculator()}
+        binding.Subtracao.setOnClickListener{calculator()}
+        binding.Multiplicacao.setOnClickListener{calculator()}
+        binding.Divisao.setOnClickListener{calculator()}
+        //TODO onClick Listener
     }
 
-class Calculadora{
-    fun Somar(n1 : Int, n2 : Int){
-        var soma = n1 + n2
+    fun calculator():Double{
+        val number1 = binding.Number1.text.toString().toDouble()
+        val number2 = binding.Number2.text.toString().toDouble()
+        val radiogroupchecked = binding.radioGroup.checkedRadioButtonId
+        val resultado = when (radiogroupchecked){
+            R.id.Soma -> calculadora().somar(number1, number2)
+            R.id.Subtracao -> calculadora().subtrair(number1, number2)
+            R.id.Multiplicacao -> calculadora().multiplicar(number1, number2)
+            else -> calculadora().dividir(number1, number2)
+        }
+        binding.Resultado.text = resultado.toString()
+        return(resultado)
+    }
+class calculadora{
+    fun somar(number1: Double, number2: Double): Double {
+        var soma = number1 + number2
         return(soma)
-
+    }
+    fun subtrair(number1: Double, number2: Double): Double {
+        var subtracao = number1 - number2
+        return(subtracao)
+    }
+    fun multiplicar(number1: Double, number2: Double): Double {
+        var multiplicacao = number1 * number2
+        return(multiplicacao)
+    }
+    fun dividir(number1: Double, number2: Double): Double {
+        var divisao = number1 / number2
+        return(divisao)
     }
 }
     /*val editTextN1 = findViewById<EditText>(R.id.Number1)
